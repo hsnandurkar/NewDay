440d290ff6667d9ae2e37fe62a4e77cb1cadff74
Hemant Nandurkar <hsnandurkar@gmail.com>
2026-06-05 00:47:57 +0530

    app.py version line change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "updated Flask sample application on azure hghapp service updated verrsion-5"
+    return "updated Flask sample application on azure hghapp service updated vernon 677"
 
 
 if __name__ == "__main__":
